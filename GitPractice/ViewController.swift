5b4c89f098a735bf5a37b6deddda1ce6422599a4
tobihige <forswift1019@gmail.com>
2023-12-26 20:28:42 +0900

    initial commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by tobihige on 2023/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
