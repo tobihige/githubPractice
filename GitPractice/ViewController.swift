3502ec2d889941e8d2e049eb5255cb82802a72ab
tobihige <forswift1019@gmail.com>
2023-12-31 20:53:45 +0900

    tableViewを追加

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    private var fruits = ["orange", "melon", "banana"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = fruits[indexPath.row]
+        return cell
+    }
+}
